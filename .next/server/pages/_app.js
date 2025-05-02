e2fe2e16b6f43d43d878add5c48e86b8ee1523a2
OmniChain <omnichain@icloud.com>
2025-05-02 09:40:29 +0000

    ✅ Static build with working index.html

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -26,7 +26,7 @@ module.exports = require("react");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [624], () => (__webpack_exec__(624)));
+var __webpack_exports__ = __webpack_require__.X(0, [35], () => (__webpack_exec__(35)));
 module.exports = __webpack_exports__;
 
 })();
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>OmniModel Dashboard</h1>
+      <p>✅ Static page rendered successfully</p>
+    </div>
+  );
+}
