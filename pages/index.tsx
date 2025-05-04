6a2087febc1c2adc9683b5af49c95914b4f9c6b7
OmniChain <omnichain@icloud.com>
2025-05-04 13:12:53 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,18 @@
 
 import React from "react";
 import ChronoMatchPanel from "../components/ChronoMatchPanel";
+import BIODEFMatchPanel from "../components/BIODEFMatchPanel";
 
 export default function Home() {
   return (
-    <div className="p-6 font-sans">
+    <div className="p-6 font-sans space-y-6">
       <h1 className="text-3xl font-bold mb-4">OmniModel Dashboard</h1>
+
+      {/* ChronoMatch Panel */}
       <ChronoMatchPanel />
+
+      {/* BIODEFMatch Panel */}
+      <BIODEFMatchPanel />
     </div>
   );
 }
