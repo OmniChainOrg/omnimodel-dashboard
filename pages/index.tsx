abf42c28e1a6108f3e15f344d41698c9729e9dbf
OmniChain <omnichain@icloud.com>
2025-05-04 03:18:44 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
+// pages/index.tsx
+
+import React from "react";
 import ChronoMatchPanel from "../components/ChronoMatchPanel";
 
-// Then somewhere inside the return:
-<ChronoMatchPanel />
+export default function Home() {
+  return (
+    <div className="p-6 font-sans">
+      <h1 className="text-3xl font-bold mb-4">OmniModel Dashboard</h1>
+      <ChronoMatchPanel />
+    </div>
+  );
+}
