3fe9933fa85032bfb16edfd6177559cfe9d9cfa6
OmniChain <omnichain@icloud.com>
2025-05-11 19:24:07 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,8 @@
 // pages/index.tsx
 
 import React from "react";
-import OmniVestPanel from "../components/OmniVestPanel";
-import ChronoMatchPanel from "../components/ChronoMatchPanel";
-import BIODEFMatchPanel from "../components/BIODEFMatchPanel";
-import LongevityZonePanel from "../components/LongevityZonePanel";
-import NEURODiagPanel from "../components/NEURODiagPanel";
-
+import OmniModelDashboard from "../components/OmniModelDashboard";
 
 export default function Home() {
-  return (
-    <div className="p-6 font-sans space-y-6">
-      <h1 className="text-3xl font-bold mb-4">OmniModel Dashboard</h1>
-
-      {/* OmniVest Panel */}
-      <OmniVestPanel />
-      
-      {/* ChronoMatch Panel */}
-      <ChronoMatchPanel />
-
-      {/* BIODEFMatch Panel */}
-      <BIODEFMatchPanel />
-      
-      {/* LongevityZone Panel */}
-      <LongevityZonePanel />
-
-      {/* NEURODiag Panel */}
-      <NEURODiagPanel />
-      
-    </div>
-  );
+  return <OmniModelDashboard />;
 }
-
-
-
-// Inside the return block:
-
-
