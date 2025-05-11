00c9cd2a1e5c26389c7674711e55e33b59e8c4aa
OmniChain <omnichain@icloud.com>
2025-05-11 17:42:50 +0200

    Update BIODEFMatchPanel.tsx

--- components/BIODEFMatchPanel.tsx
+++ components/BIODEFMatchPanel.tsx
@@ -1,6 +1,6 @@
 // components/BIODEFMatchPanel.tsx
 
-import ExpandedEvent from "../ExpandedEvent";
+import ExpandedEvent from "./ExpandedEvent";
 import React, { useEffect, useState } from "react";
 import { Card, CardContent } from "../components/ui/card";
 import { Badge } from "../components/ui/badge";
