ab0a92748a12f10e4200c2cb5a00560788f288be
OmniChain <omnichain@icloud.com>
2025-05-25 21:41:28 +0200

    Update index.tsx

--- pages/posterior /index.tsx
+++ pages/posterior /index.tsx
@@ -1,1 +1,10 @@
+import React from "react";
+import PosteriorPilotDashboard from "../components/PosteriorPilotDashboard";
 
+export default function IndexPage() {
+  return (
+    <main className="p-6">
+      <PosteriorPilotDashboard />
+    </main>
+  );
+}
