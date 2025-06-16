b9acf0558964cd91264980468e0c23444e8d348f
OmniChain <omnichain@icloud.com>
2025-06-16 13:31:49 +0200

    Create Nav.tsx

---
+++ components/Nav.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+// Inside the return JSX
+<nav className="flex space-x-4">
+  {/* other nav links */}
+  <Link href="/epistemic-engine" className="hover:underline text-blue-600">
+    🧠 Epistemic Engine
+  </Link>
+</nav>
