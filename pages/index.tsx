07d96ddea0eb9d45b0d8b7fc23a257fa5da4c5bb
OmniChain <omnichain@icloud.com>
2025-06-16 17:32:38 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,14 @@
 // pages/index.tsx
 
 import React from "react";
-import Nav from "@/components/Nav";
+import { Nav } from "@/components/Nav";
 import OmniModelDashboard from "@/components/OmniModelDashboard";
 
 export default function Home() {
   return (
     <>
       <Nav />
-      <main className="p-4">
-        <OmniModelDashboard />
-      </main>
+      <OmniModelDashboard />
     </>
   );
 }
