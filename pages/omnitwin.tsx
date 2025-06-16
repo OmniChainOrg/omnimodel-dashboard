506a6bd31a1a66b358708d46a12d928b5e1833e4
OmniChain <omnichain@icloud.com>
2025-06-16 13:56:39 +0200

    Create omnitwin.tsx

---
+++ pages/omnitwin.tsx
@@ -0,0 +1,8 @@
+// pages/omnitwin.tsx
+
+import React from "react";
+import OmniTwinNexusPanel from "@/components/OmniTwinNexusPanel";
+
+export default function OmniTwinPage() {
+  return <OmniTwinNexusPanel />;
+}
