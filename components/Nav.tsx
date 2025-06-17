f5417136e29a0e9f3162d3241128ef6d837ccf1b
OmniChain <omnichain@icloud.com>
2025-06-17 02:05:34 +0200

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 export default function Nav() {
   return (
     <nav className="flex space-x-4">
-// <Link href="/epistemic-engine" className="hover:underline text-blue-600">
-//   🧠 Epistemic Engine
-// </Link>
     </nav>
   );
 }
