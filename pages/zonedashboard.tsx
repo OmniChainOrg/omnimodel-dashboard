5120cd615ca8ad629f12e0cad5f51d61612a7a25
OmniChain <omnichain@icloud.com>
2025-07-05 18:49:08 +0200

    Update zonedashboard.tsx

--- pages/zonedashboard.tsx
+++ pages/zonedashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ZoneDashboard from '../components/ZoneDashboard';
+import { ZoneDashboard } from '../components/ZoneDashboard';
 
 const ZoneDashboardPage: React.FC = () => (
   <div className="min-h-screen bg-gray-100 p-8">
