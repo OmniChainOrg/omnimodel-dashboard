da50b64f99ccaf9ed1b8b0cf12d7a5ff23cbb42d
OmniChain <omnichain@icloud.com>
2025-07-05 18:40:54 +0200

    Update zonedashboard.tsx

--- pages/zonedashboard.tsx
+++ pages/zonedashboard.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { ZoneDashboard } from '@/components/ZoneDashboard';
+import ZoneDashboard from '../components/ZoneDashboard';
 
-const ZoneDashboardPage: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-gray-100 p-8">
-      <ZoneDashboard archetypeId="root" archetypeName="Root Zone Archetype" />
-    </div>
-  );
-};
+const ZoneDashboardPage: React.FC = () => (
+  <div className="min-h-screen bg-gray-100 p-8">
+    <ZoneDashboard archetypeId="root" archetypeName="Root Zone Archetype" />
+  </div>
+);
 
 export default ZoneDashboardPage;
