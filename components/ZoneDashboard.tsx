52fd0b18bf9c05aca1db118c059e498235e81fd7
OmniChain <omnichain@icloud.com>
2025-07-05 04:14:17 +0200

    Update ZoneDashboard.tsx

--- components/ZoneDashboard.tsx
+++ components/ZoneDashboard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import { useZoneArchetype, Zone } from '../hooks/useZoneArchetype';
 
 // Recursive Zone Node with explicit Tailwind styling
