23eee3089f985d610e0eb0d32ecf8337802880a0
OmniChain <omnichain@icloud.com>
2025-07-13 03:21:37 +0200

    Update index.tsx

--- pages/memory/index.tsx
+++ pages/memory/index.tsx
@@ -1,7 +1,6 @@
 // pages/memory/index.tsx
 
 import React, { useState, useEffect } from 'react';
-import MemoryPanel from '../../components/MemoryPanel';
 import { ZoneRegistry, approveZone, declineZone, Zone } from '../../lib/zoneRegistry';
 
 export default function MemoryPage() {
