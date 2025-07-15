1015bba589426398898e0e2b38eb3e70079e881e
OmniChain <omnichain@icloud.com>
2025-07-15 21:45:57 +0200

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,16 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="flex space-x-4">
+    <nav className="flex space-x-4 p-4 bg-gray-100 rounded">
+      <Link href="/" className="text-blue-600 hover:text-blue-800">
+        Home
+      </Link>
+      <Link href="/zonedashboard" className="text-blue-600 hover:text-blue-800">
+        Zone Dashboard
+      </Link>
+      <Link href="/zonesubdashboard" className="text-blue-600 hover:text-blue-800">
+        Zone Sub Dashboard
+      </Link>
     </nav>
   );
 }
