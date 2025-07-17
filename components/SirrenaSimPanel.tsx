73fcb6841a6ba30ffc2504f1e288d3ba7a32e595
OmniChain <omnichain@icloud.com>
2025-07-17 06:11:23 +0200

    Update SirrenaSimPanel.tsx

--- components/SirrenaSimPanel.tsx
+++ components/SirrenaSimPanel.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Zone } from '@/lib/zoneRegistry';
 import ExpandedEvent from "./ExpandedEvent";
 import { Card, CardContent } from "../components/ui/card";
 import { Badge } from "../components/ui/badge";
